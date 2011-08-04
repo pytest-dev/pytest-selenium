79508e2c14263008fe2d20c36a4ac3434845cc94
AutomatedTester <dburns@mozilla.com>
2011-08-04 16:24:48 +0100

    Broke out things into methods and added tests to check it still worked

--- mozwebqa/mozwebqa.py
+++ mozwebqa/mozwebqa.py
@@ -59,83 +59,15 @@ def pytest_runtest_setup(item):
     
     TestSetup.skip_selenium = True
 
-    if TestSetup.base_url is None:
-        raise pytest.UsageError('--base-url must be specified.')
-
-    sauce_labs = item.sauce_labs_username or item.sauce_labs_api_key
-
-    if item.api == 'webdriver':
-        if not item.browser_name:
-            raise pytest.UsageError("--browser-name must be specified when using the 'webdriver' api.")
-        if not item.browser_version:
-            raise pytest.UsageError("--browser-ver must be specified when using the 'webdriver' api.")
-        if not item.platform:
-            raise pytest.UsageError("--platform must be specified when using the 'webdriver' api.")
-    else:
-        if sauce_labs:
-            if not item.browser_name:
-                raise pytest.UsageError("--browser-name must be specified when using the 'rc' api with sauce labs.")
-            if not item.browser_version:
-                raise pytest.UsageError("--browser-ver must be specified when using the 'rc' api with sauce labs.")
-            if not item.platform:
-                raise pytest.UsageError("--platform must be specified when using the 'rc' api with sauce labs.")
-        else:
-            if not(item.browser or item.environment):
-                raise pytest.UsageError("--browser or --environment must be specified when using the 'rc' api.")
-
-    if sauce_labs:
-        if not item.sauce_labs_username:
-            raise pytest.UsageError('--sauce-user must be specified.')
-        if not item.sauce_labs_api_key:
-            raise pytest.UsageError('--sauce-key must be specified.')
+    _check_usage(item)
 
     if not 'skip_selenium' in item.keywords:
         TestSetup.skip_selenium = False
 
         if item.api == 'webdriver':
-            if item.sauce_labs_username:
-                capabilities = {
-                    'platform': item.platform,
-                    'browserName': item.browser_name,
-                    'version': item.browser_version,
-                    'name': item.keywords.keys()[0],
-                    'public': False}
-                executor = 'http://%s:%s@ondemand.saucelabs.com:80/wd/hub' % (item.sauce_labs_username, item.sauce_labs_api_key)
-                TestSetup.selenium = webdriver.Remote(command_executor = executor,
-                                                      desired_capabilities = capabilities)
-            else:
-                capabilities = getattr(webdriver.DesiredCapabilities, item.browser_name.upper())
-                capabilities['version'] = item.browser_version
-                capabilities['platform'] = item.platform.upper()
-                executor = 'http://%s:%s/wd/hub' % (item.host, item.port)
-                try:
-                    TestSetup.selenium = webdriver.Remote(command_executor = executor,
-                                                        desired_capabilities = capabilities)
-                except AttributeError:
-                    valid_browsers = [attr for attr in dir(webdriver.DesiredCapabilities) if not attr.startswith('__')]
-                    raise AttributeError("Invalid browser name: '%s'. Valid options are: %s" % (item.browser_name, ', '.join(valid_browsers)))
+            _setup_webdriver(item)
         else:
-            if item.sauce_labs_username:
-                TestSetup.selenium = selenium('ondemand.saucelabs.com', '80',
-                                              json.dumps({
-                                                'username': item.sauce_labs_username,
-                                                'access-key':  item.sauce_labs_api_key,
-                                                'os': item.platform,
-                                                'browser': item.browser_name,
-                                                'browser-version': item.browser_version,
-                                                'name': item.keywords.keys()[0],
-                                                'public': False}),
-                                              TestSetup.base_url)
-            else:
-                browser = item.environment or item.browser
-                TestSetup.selenium = selenium(item.host, str(item.port), browser, TestSetup.base_url)
-
-            if item.config.option.capture_network:
-                TestSetup.selenium.start("captureNetworkTraffic=true")
-            else:
-                TestSetup.selenium.start()
-
-            TestSetup.selenium.set_timeout(TestSetup.timeout)
+            _setup_selenium(item) 
 
 
 def pytest_runtest_teardown(item):
@@ -233,7 +165,98 @@ def pytest_addoption(parser):
                      metavar = 'path',
                      help="location of yaml file containing user credentials.")
 
+def _check_sauce_usage(item):
+    '''
+        If this is for SauceLabs usage, we need to check a few details
+    '''
+    if not item.sauce_labs_username:
+        raise pytest.UsageError('--sauce-user must be specified.')
+    if not item.sauce_labs_api_key:
+        raise pytest.UsageError('--sauce-key must be specified.')
+    if item.api is "rc":
+        if not item.browser_name:
+            raise pytest.UsageError("--browser-name must be specified when using the 'rc' api with sauce labs.")
+        if not item.browser_version:
+            raise pytest.UsageError("--browser-ver must be specified when using the 'rc' api with sauce labs.")
+        if not item.platform:
+            raise pytest.UsageError("--platform must be specified when using the 'rc' api with sauce labs.")
+
+def _check_usage(item):
+    '''
+        Check that the usage parameters are correct. If wrong throws the appropriate error
+    '''
+    if TestSetup.base_url is None:
+        raise pytest.UsageError('--base-url must be specified.')
+
+    item.sauce_labs = item.sauce_labs_username or item.sauce_labs_api_key
+    
+    if item.api == 'webdriver':
+        if not item.browser_name:
+            raise pytest.UsageError("--browser-name must be specified when using the 'webdriver' api.")
+        if not item.browser_version:
+            raise pytest.UsageError("--browser-ver must be specified when using the 'webdriver' api.")
+        if not item.platform:
+            raise pytest.UsageError("--platform must be specified when using the 'webdriver' api.")
+    else:
+        if item.sauce_labs:
+            _check_sauce_usage(item)
+        else:
+            if not(item.browser or item.environment):
+                raise pytest.UsageError("--browser or --environment must be specified when using the 'rc' api.")
+
+    if item.sauce_labs:
+        _check_sauce_usage(item)
+
+def _setup_webdriver(item):
+    if item.sauce_labs_username:
+        capabilities = {
+                    'platform': item.platform,
+                    'browserName': item.browser_name,
+                    'version': item.browser_version,
+                    'name': item.keywords.keys()[0],
+                    'public': False}
+        executor = 'http://%s:%s@ondemand.saucelabs.com:80/wd/hub' % (item.sauce_labs_username, item.sauce_labs_api_key)
+        TestSetup.selenium = webdriver.Remote(command_executor = executor,
+                                                      desired_capabilities = capabilities)
+    else:
+        capabilities = getattr(webdriver.DesiredCapabilities, item.browser_name.upper())
+        capabilities['version'] = item.browser_version
+        capabilities['platform'] = item.platform.upper()
+        executor = 'http://%s:%s/wd/hub' % (item.host, item.port)
+        try:
+            TestSetup.selenium = webdriver.Remote(command_executor = executor,
+                                                        desired_capabilities = capabilities)
+        except AttributeError:
+            valid_browsers = [attr for attr in dir(webdriver.DesiredCapabilities) if not attr.startswith('__')]
+            raise AttributeError("Invalid browser name: '%s'. Valid options are: %s" % (item.browser_name, ', '.join(valid_browsers)))
+
+def _setup_selenium(item):
+    if item.sauce_labs_username:
+        TestSetup.selenium = selenium('ondemand.saucelabs.com', '80',
+                                      json.dumps({
+                                      'username': item.sauce_labs_username,
+                                      'access-key':  item.sauce_labs_api_key,
+                                      'os': item.platform,
+                                      'browser': item.browser_name,
+                                      'browser-version': item.browser_version,
+                                      'name': item.keywords.keys()[0],
+                                      'public': False}),
+                                      TestSetup.base_url)
+    else:
+        browser = item.environment or item.browser
+        TestSetup.selenium = selenium(item.host, str(item.port), browser, TestSetup.base_url)
+
+    if item.config.option.capture_network:
+        TestSetup.selenium.start("captureNetworkTraffic=true")
+    else:
+        TestSetup.selenium.start()
+
+    TestSetup.selenium.set_timeout(TestSetup.timeout)
+
 
 class TestSetup:
+    '''
+        This class is just used for monkey patching
+    '''
     def __init__(self, request):
         self.request = request
