c703cdcb01683225a58d6e1c1927af921a94ff3d
Dave Hunt <dave.hunt@gmail.com>
2011-09-09 14:09:35 +0100

    Added tests for custom report.

---
+++ testing/test_report.py
@@ -0,0 +1,79 @@
+#!/usr/bin/env python
+# ***** BEGIN LICENSE BLOCK *****
+# Version: MPL 1.1/GPL 2.0/LGPL 2.1
+#
+# The contents of this file are subject to the Mozilla Public License Version
+# 1.1 (the "License"); you may not use this file except in compliance with
+# the License. You may obtain a copy of the License at
+# http://www.mozilla.org/MPL/
+#
+# Software distributed under the License is distributed on an "AS IS" basis,
+# WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
+# for the specific language governing rights and limitations under the
+# License.
+#
+# The Original Code is Mozilla WebQA Tests.
+#
+# The Initial Developer of the Original Code is Mozilla Foundation.
+# Portions created by the Initial Developer are Copyright (C) 2011
+# the Initial Developer. All Rights Reserved.
+#
+# Contributor(s): Dave Hunt <dhunt@mozilla.com>
+#
+# Alternatively, the contents of this file may be used under the terms of
+# either the GNU General Public License Version 2 or later (the "GPL"), or
+# the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
+# in which case the provisions of the GPL or the LGPL are applicable instead
+# of those above. If you wish to allow use of your version of this file only
+# under the terms of either the GPL or the LGPL, and not to allow others to
+# use your version of this file under the terms of the MPL, indicate your
+# decision by deleting the provisions above and replace them with the notice
+# and other provisions required by the GPL or the LGPL. If you do not delete
+# the provisions above, a recipient may use your version of this file under
+# the terms of any one of the MPL, the GPL or the LGPL.
+#
+# ***** END LICENSE BLOCK *****
+
+import os
+import pytest
+
+from webserver import SimpleWebServer
+
+def setup_module(module):
+    webserver = SimpleWebServer()
+    webserver.start()
+    TestReport.webserver = webserver
+
+def teardown_module(module):
+    TestReport.webserver.stop()
+
+@pytest.mark.skip_selenium
+class TestReport:
+
+    def testReportWithoutDirectory(self, testdir):
+        file_test = testdir.makepyfile("""
+            def test_report(mozwebqa):
+                mozwebqa.selenium.open('/')
+                assert mozwebqa.selenium.get_text('css=h1') == 'Success!'
+        """)
+        report = 'result.html'
+        reprec = testdir.inline_run('--baseurl=http://localhost:%s' % self.webserver.port, '--api=rc', '--browser=Firefox Beta on Mac OS X', '--webqareport=%s' % report, file_test)
+        passed, skipped, failed = reprec.listoutcomes()
+        assert len(passed) == 1
+        report_file = os.path.sep.join([str(testdir.tmpdir), report])
+        assert os.path.exists(report_file)
+        assert os.path.isfile(report_file)
+
+    def testReportWithDirectory(self, testdir):
+        file_test = testdir.makepyfile("""
+            def test_report(mozwebqa):
+                mozwebqa.selenium.open('/')
+                assert mozwebqa.selenium.get_text('css=h1') == 'Success!'
+        """)
+        report = 'report/result.html'
+        reprec = testdir.inline_run('--baseurl=http://localhost:%s' % self.webserver.port, '--api=rc', '--browser=Firefox Beta on Mac OS X', '--webqareport=%s' % report, file_test)
+        passed, skipped, failed = reprec.listoutcomes()
+        assert len(passed) == 1
+        report_file = os.path.sep.join([str(testdir.tmpdir), report])
+        assert os.path.exists(report_file)
+        assert os.path.isfile(report_file)
