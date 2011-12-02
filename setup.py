775be5319c712d0e306763f0786756e9081730bb
Dave Hunt <dave.hunt@gmail.com>
2011-12-02 01:20:57 +0000

    Updated version number to 0.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name = 'pytest-mozwebqa',
-    version = '0.5',
+    version = '0.6',
     description='Mozilla WebQA plugin for py.test.',
     author = 'Dave Hunt',
     author_email = 'dhunt@mozilla.com',
