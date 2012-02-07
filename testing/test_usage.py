9485f6cc42850ebbf05947670f12958f142208e8
AutomatedTester <dburns@mozilla.com>
2012-02-07 20:57:39 +0000

    updated test to check the message says it cant find the binary

--- testing/test_usage.py
+++ testing/test_usage.py
@@ -216,8 +216,9 @@ class TestUsage:
                 assert True
         """)
         reprec = testdir.inline_run('--baseurl=http://localhost:%s' % self.webserver.port, '--api=webdriver',
-            "--browsername=chrome", "--chromeoptions={'binary_location':'foo'}", file_test)
+            "--browsername=chrome","--browserver=18", "--platform=mac", "--chromeopts={\"binary_location\": \"foo\"}", file_test)
         passed, skipped, failed = reprec.listoutcomes()
         assert len(failed) == 1
+        assert "Could not find Chrome binary at: foo" in failed[0].longrepr.reprcrash.message 
 
 
