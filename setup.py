a0d6a4693c05bcde8fe478dcaa95076d434b4cc1
Dave Hunt <dave.hunt@gmail.com>
2012-02-08 11:41:50 +0000

    Update pytest dependency to 2.2.3 or higher - required for new debug features in reports.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     description='Mozilla WebQA plugin for py.test.',
     author = 'Dave Hunt',
     author_email = 'dhunt@mozilla.com',
-    install_requires = ['pytest>=2.1.3', 'selenium', 'pyyaml'],
+    install_requires = ['pytest>=2.2.3', 'selenium', 'pyyaml'],
     url = 'https://github.com/davehunt/pytest-mozwebqa',
     packages = ['mozwebqa'],
     # the following makes a plugin available to py.test
