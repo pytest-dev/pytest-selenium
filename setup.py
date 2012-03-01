a84527ca651976546f247f836302d1566b542430
Dave Hunt <dave.hunt@gmail.com>
2012-03-01 12:37:54 +0000

    Corrected license classifier.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     classifiers = [
         'Development Status :: 4 - Beta',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL)',
+        'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
         'Operating System :: POSIX',
         'Operating System :: Microsoft :: Windows',
         'Operating System :: MacOS :: MacOS X',
