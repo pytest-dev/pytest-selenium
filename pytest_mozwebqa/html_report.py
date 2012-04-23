8ed26b7b598d5345c289587638479891a0110051
Dave Hunt <dave.hunt@gmail.com>
2012-04-23 17:35:15 +0100

    Temporarily remove Sauce Labs video from HTML report due to https://bitbucket.org/hpk42/py/issue/13

--- pytest_mozwebqa/html_report.py
+++ pytest_mozwebqa/html_report.py
@@ -99,8 +99,9 @@ class HTMLReport(object):
         if not 'Passed' in result:
             additional_html = []
 
-            if hasattr(self, 'sauce_labs_job'):
-                additional_html.append(self.sauce_labs_job.video_html)
+            #py 1.4.7 has a bug that prevents <object> in html
+            #if hasattr(self, 'sauce_labs_job'):
+            #    additional_html.append(self.sauce_labs_job.video_html)
 
             if 'Screenshot' in links:
                 additional_html.append(
