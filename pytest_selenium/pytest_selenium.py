cfe7fb1a5c8992b61e62d4c87dba3f30824d6baf
Justin Potts <justinpotts@justinpotts-macbook.local>
2015-06-12 12:56:46 -0500

    Fixed flake8 issue

--- pytest_selenium/pytest_selenium.py
+++ pytest_selenium/pytest_selenium.py
@@ -70,7 +70,6 @@ def base_url(request):
 
 @pytest.fixture(scope='session', autouse=True)
 def _verify_base_url(request, base_url):
-    option = request.config.option
     if base_url:
         response = requests.get(base_url, timeout=REQUESTS_TIMEOUT)
         if response.status_code not in (200, 401):
