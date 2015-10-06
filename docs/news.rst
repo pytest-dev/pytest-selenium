fd3d73c4ce1554914841fba9e918e5944eda592c
Dave Hunt <dave.hunt@gmail.com>
2015-10-06 18:01:11 +0100

    Bump version number to 1.0b2 and update release notes

--- docs/news.rst
+++ docs/news.rst
@@ -1,6 +1,10 @@
 Release Notes
 =============
 
-**1.0b1**
+**1.0b2 (2015-10-06)**
+* Added support for non ASCII characters in log files. (#33)
+* Added support for excluding any type of debug.
+
+**1.0b1 (2015-09-08)**
 
 * Initial beta
--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='pytest-selenium',
-      version='1.0b1',
+      version='1.0b2',
       description='pytest plugin for Selenium',
       long_description=open('README.rst').read(),
       author='Dave Hunt',
