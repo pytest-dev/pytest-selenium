7a9130d431828f1145c14c774875f743cf09249a
Dave Hunt <dave.hunt@gmail.com>
2016-03-24 09:55:38 +0000

    Add Framework::Pytest to list of classifiers

--- setup.py
+++ setup.py
@@ -30,6 +30,7 @@ setup(name='pytest-selenium',
                'mozilla',
       classifiers=[
           'Development Status :: 5 - Production/Stable',
+          'Framework :: Pytest',
           'Intended Audience :: Developers',
           'License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)',
           'Operating System :: POSIX',
