5e98cae4fa9c662b396b22c76cdcec0f7c997c1e
Jim Brännlund <jimbrannlund@fastmail.com>
2017-04-22 13:33:08 +0200

    Add alternate credentials environment variables for Jenkins declarative pipelines
    Fixes #96

--- pytest_selenium/exceptions.py
+++ pytest_selenium/exceptions.py
@@ -7,8 +7,8 @@ import pytest
 
 class MissingCloudCredentialError(pytest.UsageError):
 
-    def __init__(self, driver, key, env):
+    def __init__(self, driver, key, envs):
         super(MissingCloudCredentialError, self).__init__(
-            '{0} {1} must be set. Try setting the {2} environment '
-            'variable, or see the documentation for how to use a '
-            'configuration file.'.format(driver, key, env))
+            '{0} {1} must be set. Try setting one of the following '
+            'environment variables {2}, or see the documentation for '
+            'how to use a configuration file.'.format(driver, key, envs))
