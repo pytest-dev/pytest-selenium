36f57b19de2a3343dacce019beef880f1494f19b
Benjamin Forehand Jr <bennyjr169@gmail.com>
2017-04-28 14:35:05 -0700

    Fixed flake8 error

--- testing/test_report.py
+++ testing/test_report.py
@@ -148,7 +148,7 @@ def test_exclude_debug_config(testdir, httpserver, monkeypatch, exclude):
 
 
 def test_driver_log(testdir, httpserver):
-    driver_log_regex = '<a class="text" href=".*" target="_blank">Driver Log</a>' #noqa
+    driver_log_regex = '<a class="text" href=".*" target="_blank">Driver Log</a>'  # noqa
     httpserver.serve_content(content='<h1>Success!</h1><p>Ё</p>')
     result, html = run(testdir)
     assert re.search(driver_log_regex, html) is not None
