741fc2de361a38fb7a2a9213f07be6e8dd3b7372
Tom <Mcgtom10@gmail.com>
2018-01-10 12:38:45 -0800

    Use io module to encode as UTF-8
    This allows files to contain non-ASCII characters without throwing an error.

--- pytest_selenium/pytest_selenium.py
+++ pytest_selenium/pytest_selenium.py
@@ -6,6 +6,7 @@ import argparse
 import copy
 from datetime import datetime
 import os
+import io
 
 import pytest
 from requests.structures import CaseInsensitiveDict
@@ -243,7 +244,7 @@ def _gather_logs(item, report, driver, summary, extra):
     pytest_html = item.config.pluginmanager.getplugin('html')
     if item.config._driver_log and os.path.exists(item.config._driver_log):
         if pytest_html is not None:
-            with open(item.config._driver_log, 'r') as f:
+            with io.open(item.config._driver_log, 'r', encoding = 'utf8') as f:
                 extra.append(pytest_html.extras.text(f.read(), 'Driver Log'))
         summary.append('Driver log: {0}'.format(item.config._driver_log))
     try:
