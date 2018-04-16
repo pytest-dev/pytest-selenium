d0395f8fe1a0a2b7bf602ef5c275c7d1b5c6237d
Jim Brännlund <jimbrannlund@fastmail.com>
2018-04-16 14:52:53 +0200

    took a stab at some tests

--- testing/test_saucelabs.py
+++ testing/test_saucelabs.py
@@ -4,8 +4,11 @@
 
 from functools import partial
 import os
+import datetime
 
 import pytest
+
+monkeytime = datetime.datetime(2020, 12, 25, 17)
 
 pytestmark = pytestmark = [pytest.mark.skip_selenium,
                            pytest.mark.nondestructive]
@@ -70,3 +73,44 @@ def test_invalid_credentials_file(failure, monkeypatch, tmpdir):
     out = failure()
     messages = ['Sauce Labs Authentication Error', 'basic auth failed']
     assert any(message in out for message in messages)
+
+
+@pytest.mark.parametrize(('auth_type', 'auth_token'), [('none', ''),
+                                                       ('token', '5d5b3d4fe1fcb72edf5f8c431eb10175'),
+                                                       ('day', 'e9480f85e8183e94e39fc69af685831c'),
+                                                       ('hour', '441bac7116b1eeb026fed2a0942aad57')])
+def test_auth_token(monkeypatch, auth_type, auth_token):
+    import datetime
+    from pytest_selenium.drivers.saucelabs import SauceLabs, get_job_url
+
+    monkeypatch.setattr(datetime, 'datetime', MonkeyDatetime)
+    monkeypatch.setenv('SAUCELABS_USERNAME', 'foo')
+    monkeypatch.setenv('SAUCELABS_API_KEY', 'bar')
+
+    session_id = '12345678'
+    url = 'http://saucelabs.com/jobs/{}'.format(session_id)
+
+    if auth_type != 'none':
+        url += '?auth={}'.format(auth_token)
+
+    result = get_job_url(Config(auth_type), SauceLabs(), session_id)
+    assert result == url
+
+
+class MonkeyDatetime:
+
+    @classmethod
+    def utcnow(cls):
+        return monkeytime
+
+
+class Config(object):
+
+    def __init__(self, value):
+        self._value = value
+
+    def getini(self, key):
+        if key == 'saucelabs_job_auth':
+            return self._value
+        else:
+            raise KeyError
