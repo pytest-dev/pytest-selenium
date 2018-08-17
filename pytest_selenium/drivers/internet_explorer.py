5f1c9d35d8e64b0ca899df79f38266ceb713a261
Jim Brännlund <BeyondEvil@users.noreply.github.com>
2018-08-17 23:14:35 +0200

    remove log_path and use service_log_path consistently instead (#180)
    * remove log_path and use service_log_path consistently instead
    
    * add LooseVersion for service_log_path

--- pytest_selenium/drivers/internet_explorer.py
+++ pytest_selenium/drivers/internet_explorer.py
@@ -1,14 +1,20 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
+from distutils.version import LooseVersion
+from selenium import __version__ as SELENIUM_VERSION
 
 
 def driver_kwargs(capabilities, driver_log, driver_path, **kwargs):
-    kwargs = {}
+
+    # Selenium 3.14.0 deprecated log_file in favour of service_log_path
+    if LooseVersion(SELENIUM_VERSION) < LooseVersion('3.14.0'):
+        kwargs = {'log_file': driver_log}
+    else:
+        kwargs = {'service_log_path': driver_log}
+
     if capabilities:
         kwargs['capabilities'] = capabilities
-    if driver_log is not None:
-        kwargs['log_file'] = driver_log
     if driver_path is not None:
         kwargs['executable_path'] = driver_path
     return kwargs
