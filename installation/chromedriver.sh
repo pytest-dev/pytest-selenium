3abd265be8eee913242faf0e85b2d14c622cd3b3
Jim Brännlund <jimbrannlund@fastmail.com>
2019-05-30 15:38:07 +0200

    Moar ts

--- installation/chromedriver.sh
+++ installation/chromedriver.sh
@@ -13,6 +13,9 @@ base_url="https://chromedriver.storage.googleapis.com"
 
 filename="chromedriver_${os}64.zip"
 curl -sL -o /tmp/"${filename}" "${base_url}/${version}/${filename}"
+
+[[ $(uname) == "Linux" ]] && chmod 777 /tmp/"${filename}"
+
 unzip -q /tmp/"${filename}"
 mv chromedriver "${install_dir}"
 echo "chromedriver ${version} is now available in '${install_dir}'"
