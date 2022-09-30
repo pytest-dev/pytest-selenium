0027a99ea194ccc0e31ad4f330f538677b99ab47
Jim Brännlund <jim.brannlund@gmail.com>
2022-10-01 01:27:04 +0200

    chore: Remove Selenium 3 deprecations

--- src/pytest_selenium/drivers/chrome.py
+++ src/pytest_selenium/drivers/chrome.py
@@ -1,23 +1,18 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from packaging.version import Version
-
 import pytest
-from selenium import __version__ as SELENIUM_VERSION
 from selenium.webdriver.chrome.options import Options
 
 
 def driver_kwargs(
     capabilities, driver_args, driver_log, driver_path, chrome_options, **kwargs
 ):
-    kwargs = {"desired_capabilities": capabilities, "service_log_path": driver_log}
-
-    # Selenium 3.8.0 deprecated chrome_options in favour of options
-    if Version(SELENIUM_VERSION) < Version("3.8.0"):
-        kwargs["chrome_options"] = chrome_options
-    else:
-        kwargs["options"] = chrome_options
+    kwargs = {
+        "desired_capabilities": capabilities,
+        "service_log_path": driver_log,
+        "options": chrome_options,
+    }
 
     if driver_args is not None:
         kwargs["service_args"] = driver_args
--- src/pytest_selenium/drivers/internet_explorer.py
+++ src/pytest_selenium/drivers/internet_explorer.py
@@ -1,17 +1,11 @@
 # This Source Code Form is subject to the terms of the Mozilla Public
 # License, v. 2.0. If a copy of the MPL was not distributed with this
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
-from packaging.version import Version
-from selenium import __version__ as SELENIUM_VERSION
 
 
 def driver_kwargs(capabilities, driver_log, driver_path, **kwargs):
 
-    # Selenium 3.14.0 deprecated log_file in favour of service_log_path
-    if Version(SELENIUM_VERSION) < Version("3.14.0"):
-        kwargs = {"log_file": driver_log}
-    else:
-        kwargs = {"service_log_path": driver_log}
+    kwargs = {"service_log_path": driver_log}
 
     if capabilities:
         kwargs["capabilities"] = capabilities
